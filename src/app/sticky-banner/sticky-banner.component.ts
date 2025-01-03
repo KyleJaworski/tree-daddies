898f69b7852253180e1436a48cb0c77ed306a35a
Kyle Jaworski <KyleJaworski1@gmail.com>
2025-01-03 14:25:22 -0500

    fixed content orientation

--- src/app/sticky-banner/sticky-banner.component.ts
+++ src/app/sticky-banner/sticky-banner.component.ts
@@ -3,21 +3,18 @@ import { Menubar } from 'primeng/menubar';
 
 import { RouterModule } from '@angular/router'; // Import RouterModule
 
-
 @Component({
   selector: 'app-sticky-banner',
-  imports: [ RouterModule, Menubar],
+  imports: [RouterModule, Menubar],
   templateUrl: './sticky-banner.component.html',
-  styleUrl: './sticky-banner.component.scss'
+  styleUrl: './sticky-banner.component.scss',
 })
 export class StickyBannerComponent {
-
-
   items = [
     { label: 'About Us', fragment: 'about', icon: 'pi pi-info-circle' },
     { label: 'Services', fragment: 'services', icon: 'pi pi-cog' },
     { label: 'Contact', fragment: 'contact', icon: 'pi pi-envelope' },
     { label: 'Book', fragment: 'book', icon: 'pi pi-calendar-plus' },
-    { label: 'FAQ', fragment: 'faq', icon: 'pi pi-question-circle' }
-];
+    { label: 'FAQ', fragment: 'faq', icon: 'pi pi-question-circle' },
+  ];
 }
