ae78bf3f7568d4377b016730f2e6177e0e506f91
Kyle Jaworski <KyleJaworski1@gmail.com>
2025-01-03 16:15:25 -0500

    mobile formatting

--- src/app/sticky-banner/sticky-banner.component.ts
+++ src/app/sticky-banner/sticky-banner.component.ts
@@ -1,20 +1,40 @@
 import { Component } from '@angular/core';
 import { Menubar } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 import { RouterModule } from '@angular/router'; // Import RouterModule
 
 @Component({
   selector: 'app-sticky-banner',
-  imports: [RouterModule, Menubar],
+  imports: [
+    RouterModule,
+    Menubar,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    CommonModule,
+  ],
   templateUrl: './sticky-banner.component.html',
   styleUrl: './sticky-banner.component.scss',
 })
 export class StickyBannerComponent {
-  items = [
-    { label: 'About Us', fragment: 'about', icon: 'pi pi-info-circle' },
-    { label: 'Services', fragment: 'services', icon: 'pi pi-cog' },
-    { label: 'Contact', fragment: 'contact', icon: 'pi pi-envelope' },
-    { label: 'Book', fragment: 'book', icon: 'pi pi-calendar-plus' },
-    { label: 'FAQ', fragment: 'faq', icon: 'pi pi-question-circle' },
+  deskTopItems: MenuItem[] = [
+    { label: 'About Us', icon: 'pi pi-info-circle', routerLink: '' },
+    { label: 'Services', icon: 'pi pi-cog', routerLink: '' },
+    { label: 'Contact', icon: 'pi pi-envelope', routerLink: '' },
+    { label: 'Book', icon: 'pi pi-calendar-plus', routerLink: '' },
+    { label: 'FAQ', icon: 'pi pi-question-circle', routerLink: '' },
+  ];
+
+  mobileItems = [
+    { label: 'About Us', icon: 'info', routerLink: '' },
+    { label: 'Services', icon: 'carpenter', routerLink: '' },
+    { label: 'Contact', icon: 'contact_phone', routerLink: '' },
+    { label: 'Book', icon: 'event_upcoming', routerLink: '' },
+    { label: 'FAQ', icon: 'help', routerLink: '' },
   ];
 }
