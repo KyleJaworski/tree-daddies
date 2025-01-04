a20128ab5714994ad4b8821a52ac133e5465e75e
Kyle Jaworski <KyleJaworski1@gmail.com>
2025-01-04 12:13:47 -0500

    button functions

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -29,6 +29,19 @@ import { BookingSectionComponent } from '../booking-section/booking-section.comp
 export class ContactComponent {
   constructor(private dialog: MatDialog) {}
 
+  makeCall(): void {
+    window.location.href = 'tel:+17656050530';
+  }
+
+  sendEmail(): void {
+    console.log('Email button clicked');
+    const recipient = 'KyleJaworski1@gmail.com';
+    const subject = encodeURIComponent('Inquiry');
+    const body = encodeURIComponent(
+      'Hello, I would like to know more about...'
+    );
+    window.location.href = `mailto:${recipient}?subject=${subject}&body=${body}`;
+  }
   openBookingDialog() {
     const dialogRef = this.dialog.open(BookingSectionComponent);
 
